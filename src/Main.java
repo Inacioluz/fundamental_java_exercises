6fec92da910fb6d745daf44949b103c180ecaa09
Inacio Silva <joseinaciolds@gmail.com>
2024-11-19 04:12:50 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Carro meuCarro =  new Carro();
+
+        meuCarro.getMarca();
+        meuCarro.getModelo();
+        meuCarro.getAno();
+
+        meuCarro.exibitDetalhes();
+    }
+}
